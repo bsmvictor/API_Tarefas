2ef480d74234b6e9ba784a4526ea099e0eb5299e
bsmvictor <victor.boaventura@gec.inatel.br>
2025-02-04 11:42:45 -0300

    gerenciamento de dependencias e conexão com MySQL

---
+++ ApiTarefas/Database/TarefasContext.cs
@@ -0,0 +1,13 @@
+using ApiTarefas.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiTarefas.Database;
+
+public class TarefasContext : DbContext
+{
+    #nullable disable
+    
+    public TarefasContext(DbContextOptions<TarefasContext> options) : base(options) { }
+    
+    public DbSet<Tarefa> Tarefas { get; set; } = default!;
+}
--- ApiTarefas/Program.cs
+++ ApiTarefas/Program.cs
@@ -1,4 +1,15 @@
+using ApiTarefas.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Adicionando serviços de banco de dados
+builder.Services.AddDbContext<TarefasContext>(options =>
+{
+    options.UseMySql(connectionString, new MySqlServerVersion(new Version(9,2,0)));
+});
 
 // Adicionando serviços de controllers
 builder.Services.AddControllers();
